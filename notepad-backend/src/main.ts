89457cc79d650a0ce2a5b4e1cfa3d1b5227c7794
Evgeny Krivolapov <sheka1991_91@mail.ru>
2025-05-13 15:04:58 +0300

    сгенерированы модули auth и user + настройки по jwt

--- notepad-backend/src/main.ts
+++ notepad-backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookiePasrser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api')
+  app.use(cookiePasrser())
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+    exposedHeaders: 'set-cookie'
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
