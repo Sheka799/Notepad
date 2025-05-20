450de16652c5f8ec1baa0d81072f8cf4a69231b4
Evgeny Krivolapov <sheka1991_91@mail.ru>
2025-05-20 11:00:58 +0300

    Настроены серверные cookie и контроллеры для авторизации

--- notepad-backend/src/auth/auth.service.ts
+++ notepad-backend/src/auth/auth.service.ts
@@ -3,9 +3,13 @@ import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import { AuthDto } from './dto/auth.dto';
 import { verify } from 'argon2';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
+  EXPIRE_DAY_REFRESH_TOKEN = 1
+  REFRESH_TOKEN_NAME = 'refreshToken' 
+
   constructor(
     private jwt: JwtService,
     private userService: UserService
@@ -38,6 +42,21 @@ export class AuthService {
     }
   }
 
+  async getNewTokens(refreshToken: string) {
+    const result = await this.jwt.verifyAsync(refreshToken)
+    if (!result) throw new UnauthorizedException('Invalid refresh token')
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const {password, ...user} = await this.userService.getById(result.id)
+
+    const tokens = this.issueTokens(user.id)
+
+    return {
+      user,
+      ...tokens
+    }
+  }
+
   private issueTokens(userId: string) {
     const data = {id: userId}
 
@@ -63,4 +82,29 @@ export class AuthService {
     
     return user
   }
+
+  addRefreshTokenToResponse(res: Response, refreshToken: string) {
+    const expiresIn = new Date()
+    expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+
+    res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+      httpOnly: true,
+      domain: 'localhost',
+      expires: expiresIn,
+      secure: true,
+      // lax if production
+      sameSite: 'none'
+    })
+  }
+
+  removeRefreshTokenFromResponse(res: Response) {
+    res.cookie(this.REFRESH_TOKEN_NAME, '', {
+      httpOnly: true,
+      domain: 'localhost',
+      expires: new Date(0),
+      secure: true,
+      // lax if production
+      sameSite: 'none'
+    })
+  } 
 }
